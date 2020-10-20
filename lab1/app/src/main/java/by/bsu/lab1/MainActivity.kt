9b9d170cc0e0139ae8862b2031eaf14bac0373c7
Alexey Tukholko <a.tukholko@gmail.com>
2020-10-20 20:25:20 +0300

    property added

--- lab1/app/src/main/java/by/bsu/lab1/MainActivity.kt
+++ lab1/app/src/main/java/by/bsu/lab1/MainActivity.kt
@@ -7,19 +7,21 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.form.*
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.form)
-        val reqCode = 10;
+
         ok_button.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("valueFirstActivity", enter_value.text.toString())
-            startActivityForResult(intent, reqCode)
+            startActivityForResult(intent, REQUEST_CODE)
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 10) {
+        if (requestCode == REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 val result = data?.getStringExtra("result")
                 value.text = result
