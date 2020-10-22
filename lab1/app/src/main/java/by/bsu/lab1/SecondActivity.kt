b587fc06255732794ee7dc16a3275474c175dbff
Alexey Tukholko <a.tukholko@gmail.com>
2020-10-22 09:13:00 +0300

    secondActivity changed

--- lab1/app/src/main/java/by/bsu/lab1/SecondActivity.kt
+++ lab1/app/src/main/java/by/bsu/lab1/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.form.*
 
 class SecondActivity : AppCompatActivity() {
@@ -14,10 +15,10 @@ class SecondActivity : AppCompatActivity() {
         val intent = intent
         value.text = intent.getStringExtra("valueFirstActivity")
         val firstVal = if(value.text.isNotEmpty()) value.text.toString().toInt() else 0
-        val secondVal = if(enter_value.text.isNotEmpty()) enter_value.text.toString().toInt() else 0
-        val sum = firstVal + secondVal
 
         ok_button.setOnClickListener {
+            val secondVal = if(enter_value.text.isNotEmpty()) enter_value.text.toString().toInt() else 0
+            val sum = firstVal + secondVal
             val returnIntent = Intent().apply {
                 putExtra("result", sum.toString())
             }
